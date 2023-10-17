cc00bf1a026341bf99bdb25b56681a2333aa6f71
idiotekk <cheziliang@gmail.com>
2023-10-16 23:42:21 -0400

    ddcstore

--- scripts/purp.py
+++ scripts/purp.py
@@ -35,69 +35,81 @@ kwenta = Kwenta(
     network_id=10  # 420 for OP goerli testnet
 )
 
-app.layout = html.Div([
-    html.H1(
-        children=" ".join(['PURP DASHBOARD']),
-        style={'textAlign':'center'},
-    ),
-    dcc.Input(
-        id='input-token-addr',
-        style={"margin": "1% 1% 1% 1%", "width": "80%"},
-        placeholder="input trader address",
-        debounce=False,
-    ),
-    dcc.Graph(
-        id='pnl-chart',
-        responsive=True,
-        style={"margin": "1% 1% 1% 1%"},
-    ),
-    html.Div(
-        DataTable(
-            id='tx-table',
-            data=[],
-            page_size=10,
-            sort_action="native",
-            style_cell={
-                'textAlign': 'left'
-            },
-            style_header={
-                'backgroundColor': 'rgb(30, 30, 30)',
-                'color': 'white'
-            },
-            style_data={
-                'backgroundColor': 'rgb(50, 50, 50)',
-                'color': 'white'
+app.layout = html.Div(
+    [
+        dcc.Store(id='memory'),
+        html.H1(
+            children=" ".join(['PURP DASHBOARD']),
+            style={
+                'textAlign': 'center',
             },
         ),
-        style={"overflow": "scroll", "margin": "1% 1% 1% 1%"},
-    )
-],
-style={"margin": "10% 20% 10% 20%"},
+        dcc.Input(
+            id='input-addr',
+            placeholder="input trader address",
+            debounce=True,
+            style={
+                "margin": "1% 1% 1% 1%",
+                "width": "80%",
+            },
+        ),
+        dcc.Graph(
+            id='pnl-chart',
+            responsive=True,
+            style={
+                "margin": "1% 1% 1% 1%",
+            },
+        ),
+        html.Div(
+            DataTable(
+                id='tx-table',
+                data=[],
+                page_size=10,
+                sort_action="native",
+                style_cell={
+                    'textAlign': 'left',
+                },
+                style_header={
+                    'backgroundColor': 'rgb(30, 30, 30)',
+                    'color': 'white',
+                },
+                style_data={
+                    'backgroundColor': 'rgb(50, 50, 50)',
+                    'color': 'white',
+                },
+            ),
+            style={
+                "overflow": "scroll",
+                "margin": "1% 1% 1% 1%",
+            },
+        ),
+    ],
+    style={"margin": "10% 20% 10% 20%"},
 )
 
 
-@lru_cache(maxsize=128)
+@callback(
+    Output('memory', 'data'),
+    Input('input-addr', 'value')
+)
 def get_positions_for_account(account):
     df = asyncio.run(kwenta.queries.positions_for_account(account=account))
-    df["time"] = pd.to_datetime(df["timestamp"], unit="s")
-    df["open_time"] = pd.to_datetime(df["open_timestamp"], unit="s", utc=True)
-    df["close_time"] = pd.to_datetime(df["close_timestamp"], unit="s", utc=True)
-    df["duration"] = (df["close_time"].fillna(pd.Timestamp.utcnow()) - df["open_time"]).fillna("").astype(str)
     df["asset"] = df["market_key"].apply(lambda x: x[1:-4])
     df["status"] = np.where(df["is_open"], "OPEN", "CLOSED")
     df = df.sort_values("timestamp")
-    return df
+    return df.to_dict("records")
 
 
 @callback(
     Output('pnl-chart', 'figure'),
-    Input('input-token-addr', 'value')
+    Input('memory', 'data'),
 )
-def update_graph(addr: str) -> go.Figure:
+def update_graph(data) -> go.Figure:
     """
     Plot price chart for `addr`.
     """
-    df = get_positions_for_account(account=addr)
+    df = pd.DataFrame.from_records(data)
+    df["time"] = pd.to_datetime(df["timestamp"], unit="s")
     df["pnl"] = df["pnl_with_fees_paid"].cumsum()
     
     pnl = df[["time", "pnl"]].copy()
@@ -120,17 +132,21 @@ def update_graph(addr: str) -> go.Figure:
     fig.update_layout(
         template="plotly_dark",
     )
+    print("updating graph ------------------------") #debug
     return fig
 
 
 @app.callback(
     [Output("tx-table", "data"), Output('tx-table', 'columns')],
-    Input('input-token-addr', 'value')
+    Input('memory', 'data'),
 )
-def update_table(addr: str):
+def update_table(data):
     
-    df = get_positions_for_account(account=addr)
+    df = pd.DataFrame.from_records(data)
     df = df.sort_values("timestamp", ascending=False)
+    df["open_time"] = pd.to_datetime(df["open_timestamp"], unit="s", utc=True)
+    df["close_time"] = pd.to_datetime(df["close_timestamp"], unit="s", utc=True)
+    df["duration"] = (df["close_time"].fillna(pd.Timestamp.utcnow()) - df["open_time"]).fillna("").astype(str)
     for c in ["open_time", "close_time"]:
         df[c] = df[c].dt.strftime("%Y%m%d %H:%M:%S")
     pprint(df.iloc[0].to_dict())
@@ -145,6 +161,7 @@ def update_table(addr: str):
     ]
     [_.update({"id": _["name"]}) for _ in columns]
     display_cols = [_["name"] for _ in columns]
+    print("updating table ------------------") #debug
     return [df[display_cols].to_dict("records"), columns]
 
 if __name__ == '__main__':
